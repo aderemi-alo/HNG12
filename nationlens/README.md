687643c1fb7dd95c633f59ba5be0424a352cf3da
aderemi-alo <aloaderemi1@gmail.com>
2025-02-11 10:51:52 +0100

    CReated the nationlens application

---
+++ nationlens/README.md
@@ -0,0 +1,3 @@
+# nationlens
+
+A flutter project that showcases Country details from around the world.
---
+++ nationlens/android/app/src/main/kotlin/com/example/nationlens/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nationlens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
